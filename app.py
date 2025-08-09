7c1d12e0dd27c3d0f6cfb09ebc3394e8094e6113
imzombier <kona.krishna1997@gmail.com>
2025-08-09 09:29:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -388,4 +388,5 @@ def pending_count():
     return jsonify({"pending": cnt})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
